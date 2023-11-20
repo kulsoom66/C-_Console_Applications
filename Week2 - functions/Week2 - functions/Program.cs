59a794f0514bc5cfa3e00c5adac51da1f4961c61
kulsoom66 <kulsoomshoukat66@gmail.com>
2023-11-20 09:10:06 +0400

    added some changes

---
+++ Week2 - functions/Week2 - functions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week2___functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
