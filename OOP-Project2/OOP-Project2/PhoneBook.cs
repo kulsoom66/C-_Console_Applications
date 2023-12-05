7877122526aa920f39f5a9fcfba18cde9f971c2c
kulsoom66 <kulsoomshoukat66@gmail.com>
2023-12-05 12:07:10 +0400

    added phonebook

---
+++ OOP-Project2/OOP-Project2/PhoneBook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class PhoneBook
+{
+
+	string[] Names;
+	long[] Numbers;
+	int size;
+
+	public PhoneBook(int _size)
+	{
+		this.size = _size;
+		Names = new string[_size];
+		Numbers = new long[_size];
+	}
+
+	//Indexer
+	public string this[int index]
+	{
+		get { return Names[index]; }
+		set { Names[index] = value; }
+	}
+
+	public void SetEntry(int index, string name, long numbers)
+	{
+		Names[index] = name;
+		Numbers[index] = numbers;
+	}
+
+	//Get index/number of the name
+	public int getNumber(string Name)
+	{
+		for (int i =0; i<Names.Length;i++)
+		{
+			if (Names[i] == Name)
+			{
+				return i;
+			}
+		
+		}
+        return -1;//Not found
+    }
+
+
+}
---
+++ OOP-Project2/OOP-Project2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            PhoneBook phonebook1 = new PhoneBook(3);
+            phonebook1.SetEntry(0,"Kulthoom", 2);
+            phonebook1.SetEntry(1, "Saba", 3);
+
+            Console.WriteLine("The index of kulthoom is: " + phonebook1.getNumber("Kulthoom"));
+
+
+           
+        }
+    }
+}
