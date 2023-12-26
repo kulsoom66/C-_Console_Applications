a61ec9efb3ea7cbbefcf5c01725e7377dc60d244
kulsoom66 <kulsoomshoukat66@gmail.com>
2023-12-26 13:52:30 +0400

    added changes

---
+++ DB connection/DB connection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DB_connection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Session1_OOP/Session1_OOP/TypeA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session1_OOP
+{
+    //What you can write inside the namespace?
+    // 1. Class
+    // 2. Strcut [Stands for sturcture]
+    // 3. Interface
+    // 4. Enum
+
+
+    // Allowed access modifiers inside the namespace?
+    // 1. Internal ==> accesable inside the project itself, cannot be imported to other projects
+    // 2. Public ==> defualt access modifier
+
+
+    internal class TypeA
+    {
+
+    }
+}
